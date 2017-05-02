ac2bad258e339bea26d8743bc565312898c2c6e4
tanner <tanner@localhost.localdomain>
2017-05-02 13:16:18 -0400

    Start over

--- include/foo.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FOO_H_
-#define _FOO_H_
-
-int foo_main(int argc, char **argv);
-
-#endif /* _FOO_H_ */
--- src/main_foo.c
+++
@@ -1,6 +0,0 @@
-#include "../include/foo.h"
-
-int
-main(int argc, char **argv) {
-  return foo_main(argc, argv);
-}
